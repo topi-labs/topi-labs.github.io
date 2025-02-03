167a2ccd0586c1ac7b99303ee6a9f09f56fa34a5
Aditya Kulkarni <aditya.kulkarni@uconn.edu>
2025-02-03 16:27:57 -0500

    chore: first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function createParticles() {
+    const container = document.getElementById('particles');
+    for (let i = 0; i < 50; i++) {
+        const particle = document.createElement('div');
+        particle.className = 'particle';
+        particle.style.left = Math.random() * 100 + '%';
+        particle.style.top = Math.random() * 100 + '%';
+        particle.style.animationDelay = Math.random() * 3 + 's';
+        container.appendChild(particle);
+    }
+}
+
+createParticles(); 
